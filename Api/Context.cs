9d22ca6ead90837bd51b8bafeda32b2bd150c2a6
Peter Miskolczi (Junior) <72017244+breadking99@users.noreply.github.com>
2025-11-07 14:33:17 +0100

    Add project files.

---
+++ Api/Context.cs
@@ -0,0 +1,31 @@
+﻿using Api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Api;
+
+public class Context(DbContextOptions<Context> options)
+    : IdentityDbContext<User>(options)
+{
+}
+
+public class ContextFactory : IDesignTimeDbContextFactory<Context>
+{
+    public Context CreateDbContext(string[] args)
+    {
+        // Build config to read connection string
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.Development.json", optional: true)
+            .AddJsonFile("appsettings.json", optional: true)
+            .Build();
+
+        var connectionString = config.GetConnectionString("DefaultConnection");
+
+        var optionsBuilder = new DbContextOptionsBuilder<Context>();
+        optionsBuilder.UseSqlite(connectionString);
+
+        return new Context(optionsBuilder.Options);
+    }
+}
---
+++ Api/Program.cs
@@ -0,0 +1,54 @@
+﻿using Api;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Api.Models;
+
+/* Notes:
+# Database (Package Manager Consoler)
+- Install: dotnet tool install --global dotnet-ef
+- Add migration: Add-Migration InitialCreate
+- Update database: Update-Database
+ */
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add connection string and DbContext
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
+builder.Services.AddDbContext<Context>(options =>
+{
+    options.UseSqlite(connectionString);
+});
+
+// Add user identity:
+builder.Services.AddDefaultIdentity<User>(options =>
+{
+    options.User.AllowedUserNameCharacters = "aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyzAÁBCDEÉFGHIÍJKLMNOÓÖŐPQRSTUÚÜŰVWXYZ0123456789-._@+ ";
+    options.User.RequireUniqueEmail = false;
+    options.SignIn.RequireConfirmedAccount = false;
+})
+.AddEntityFrameworkStores<Context>();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
