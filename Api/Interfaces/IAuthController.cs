cd6a2aee817c80ff7c67612c6d45d43da4ceb844
Peter Miskolczi (Junior) <72017244+breadking99@users.noreply.github.com>
2025-11-09 19:08:52 +0100

    added blazor web project

---
+++ Api/Interfaces/IAuthController.cs
@@ -0,0 +1,13 @@
+﻿using Api.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Interfaces;
+
+public interface IAuthController
+{
+    //! login
+    Task<ActionResult<string>> PostLoginAsync(LoginRequest request);
+    //! register
+    Task<ActionResult<string>> PostRegisterAsync(RegisterRequest request);
+
+}
