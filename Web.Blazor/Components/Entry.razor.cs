be3acdfd8e38d364008b9e501eb885c3479d4f4b
Peter Miskolczi (Junior) <72017244+breadking99@users.noreply.github.com>
2025-11-11 16:49:29 +0100

    setting version to .net 9

--- Web.Blazor/Components/Entry.razor.cs
+++ Web.Blazor/Components/Entry.razor.cs
@@ -9,6 +9,7 @@ public partial class Entry
     [Parameter] public string Type { get; set; } = "text"; 
     [Parameter] public EventCallback<string> ValueChanged { get; set; }
     [Parameter] public string Value { get => field; set => SetValue(ref field, value); }
+    private string field = string.Empty;
 
     private void SetValue(ref string field, string value)
     {
